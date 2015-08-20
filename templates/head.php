f6ebac6250bb71c2da4e6a4a7c79f190a47050f1
mathetos <reachme@mattcromwell.com>
2015-08-20 07:03:05 -0700

    Font updates

--- templates/head.php
+++ templates/head.php
@@ -4,4 +4,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
   <?php //get_template_part( 'templates/customizer' ); ?>
+  <script type="text/javascript">
+  jQuery(document).ready(function( $ ) {
+    $(".page-header h1").each(function(){
+      $(this).text($(this).text().replace(/ /g, "_"));
+    });
+    $("h1.entry-title").each(function(){
+      $(this).text($(this).text().replace(/ /g, "_"));
+    });
+  });
+  </script>
 </head>
